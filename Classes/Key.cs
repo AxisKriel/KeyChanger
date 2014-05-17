325c4ddcd4167fb59228ddc0b7a4e030c0186828
Enerdy <rodrigo.rente@live.com>
2014-05-17 13:52:30 +0100

    v2.0 - Update and Rewrite
     - Updated for TSApi v1.2.4.1 & TShock v1.16
     - Rewrote whole plugin. Feels way more organized now.
     - Changed few messages.
     - /key mode now changes the config to match, allowing the setting to persist between sessions.
     - Added KeyChangerConfig.json example to DLL.

---
+++ Classes/Key.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KeyChanger
+{
+	public class Key
+	{
+		public string Name { get; set; }
+		public KeyTypes Type { get; set; }
+		public TShockAPI.DB.Region Region { get; set; }
+		public bool Enabled { get; set; }
+		public List<Terraria.Item> Items { get; set; }
+
+		public Key(string name, KeyTypes type, bool enabled)
+		{
+			this.Name = name;
+			this.Type = type;
+			this.Enabled = enabled;
+
+			this.Region = null;
+			this.Items = new List<Terraria.Item>();
+		}
+
+		public static Key Temple = new Key("temple", KeyTypes.Temple, Config.contents.EnableTempleKey);
+		public static Key Jungle = new Key("jungle", KeyTypes.Jungle, Config.contents.EnableJungleKey);
+		public static Key Corruption = new Key("corruption", KeyTypes.Corruption, Config.contents.EnableCorruptionKey);
+		public static Key Crimson = new Key("crimson", KeyTypes.Crimson, Config.contents.EnableCrimsonKey);
+		public static Key Hallowed = new Key("hallowed", KeyTypes.Hallowed, Config.contents.EnableHallowedKey);
+		public static Key Frozen = new Key("frozen", KeyTypes.Frozen, Config.contents.EnableHallowedKey);
+	}
+}
---
+++ Utils.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+using TShockAPI.DB;
+using Terraria;
+
+namespace KeyChanger
+{
+	class Utils
+	{
+		#region InitKeys
+		/// <summary>
+		/// Loads all keys.
+		/// </summary>
+		public static void InitKeys()
+		{
+			Key.Temple = LoadKey(KeyTypes.Temple, TShock.Regions.GetRegionByName(Config.contents.TempleRegion));
+			Key.Jungle = LoadKey(KeyTypes.Jungle, TShock.Regions.GetRegionByName(Config.contents.JungleRegion));
+			Key.Corruption = LoadKey(KeyTypes.Corruption, TShock.Regions.GetRegionByName(Config.contents.CorruptionRegion));
+			Key.Crimson = LoadKey(KeyTypes.Crimson, TShock.Regions.GetRegionByName(Config.contents.CrimsonRegion));
+			Key.Hallowed = LoadKey(KeyTypes.Hallowed, TShock.Regions.GetRegionByName(Config.contents.HallowedRegion));
+			Key.Frozen = LoadKey(KeyTypes.Frozen, TShock.Regions.GetRegionByName(Config.contents.FrozenRegion));
+		}
+		#endregion
+
+		#region LoadKey
+		/// <summary>
+		/// Loads a key from KeyChangerConfig.json.
+		/// </summary>
+		/// <param name="type">The type of key to load.</param>
+		/// <returns>The key with all the required data.</returns>
+		public static Key LoadKey(KeyTypes type, Region region = null)
+		{
+			Key key;
+			switch (type)
+			{
+				case KeyTypes.Temple:
+					key = new Key("temple", KeyTypes.Temple, Config.contents.EnableTempleKey);
+					key.Items = GetItems(Config.contents.TempleKeyItem);
+					key.Region = TShock.Regions.GetRegionByName(Config.contents.TempleRegion);
+					break;
+				case KeyTypes.Jungle:
+					key = new Key("jungle", KeyTypes.Jungle, Config.contents.EnableJungleKey);
+					key.Items = GetItems(Config.contents.JungleKeyItem);
+					key.Region = TShock.Regions.GetRegionByName(Config.contents.JungleRegion);
+					break;
+				case KeyTypes.Corruption:
+					key = new Key("corruption", KeyTypes.Corruption, Config.contents.EnableCorruptionKey);
+					key.Items = GetItems(Config.contents.CorruptionKeyItem);
+					key.Region = TShock.Regions.GetRegionByName(Config.contents.CorruptionRegion);
+					break;
+				case KeyTypes.Crimson:
+					key = new Key("crimson", KeyTypes.Crimson, Config.contents.EnableCrimsonKey);
+					key.Items = GetItems(Config.contents.CrimsonKeyItem);
+					key.Region = TShock.Regions.GetRegionByName(Config.contents.CrimsonRegion);
+					break;
+				case KeyTypes.Hallowed:
+					key = new Key("hallowed", KeyTypes.Hallowed, Config.contents.EnableHallowedKey);
+					key.Items = GetItems(Config.contents.HallowedKeyItem);
+					key.Region = TShock.Regions.GetRegionByName(Config.contents.HallowedRegion);
+					break;
+				case KeyTypes.Frozen:
+					key = new Key("frozen", KeyTypes.Frozen, Config.contents.EnableFrozenKey);
+					key.Items = GetItems(Config.contents.FrozenKeyItem);
+					key.Region = TShock.Regions.GetRegionByName(Config.contents.FrozenRegion);
+					break;
+				default:
+					return null;
+			}
+			return key;
+		}
+		#endregion
+
+		#region GetItems
+		/// <summary>
+		/// Returns a list of Terraria.Item from a list of Item ids.
+		/// </summary>
+		/// <param name="id">The int[] containing the Item ids.</param>
+		/// <returns>List[Item]</returns>
+		public static List<Item> GetItems(int[] id)
+		{
+			List<Item> list = new List<Item>();
+			foreach (int item in id)
+			{
+				list.Add(TShock.Utils.GetItemById(item));
+			}
+			return list;
+		}
+		#endregion
+
+		#region ErrorMessageHandler
+		/// <summary>
+		/// Handles error messages thrown by erroneous / lack of parameters by checking a player's group permissions.
+		/// </summary>
+		/// <param name="ply">The player executing the command.</param>
+		/// <returns>A string matching the error message.</returns>
+		public static string ErrorMessage(TSPlayer ply)
+		{
+			string error;
+			var list = new List<string>()
+			{
+				ply.Group.HasPermission("key.change") ? "change" : null,
+				ply.Group.HasPermission("key.reload") ? "reload" : null,
+				ply.Group.HasPermission("key.mode") ? "mode" : null,
+				"list"
+			};
+
+			string valid = string.Join("/", list.Where(i => i != null));
+			error = string.Format("Invalid syntax! Proper syntax: /key <{0}> [type]", valid);
+			return error;
+		}
+		#endregion
+	}
+}
